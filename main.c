108089ce195036757a3ecff1be049fa5e5d018d7
jonny-exe <69385502+Jonny-exe@users.noreply.github.com>
2021-07-27 16:08:04 +0200

    added compression with gzip

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
-#include<math.h>
-#include<stdio.h>
-#include<dirent.h>
-#include<stdlib.h>
-#include<string.h>
+#include <dirent.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 void readFolderFiles();
+int compressFile();
+
 int main() {
   char files[10 * sizeof(char)][100];
   readFolderFiles(".", files);
+  char filename[20] = "./test/testfiles";
+  compressFile(filename);
   return 0;
 }
 
@@ -16,12 +20,12 @@ void readFolderFiles(char *dirname, char **files) {
   DIR *folder;
   int idx = 0;
   struct dirent *entry;
-  
+
   folder = opendir(dirname);
   if (folder == NULL) {
     puts("No dir\n");
   } else {
-    while ((entry=readdir(folder))) {
+    while ((entry = readdir(folder))) {
       char *name = entry->d_name;
       if (name[0] == '.') {
         continue;
@@ -35,7 +39,9 @@ void readFolderFiles(char *dirname, char **files) {
       if (entry->d_type == 4) {
         char *filesInsideFolder[MAX_FILENAME_LENGTH * sizeof(char)];
         readFolderFiles(fullName, filesInsideFolder);
-        for (int i = 0; i < sizeof(filesInsideFolder) / MAX_FILENAME_LENGTH / sizeof(char);  i++) {
+        for (int i = 0;
+             i < sizeof(filesInsideFolder) / MAX_FILENAME_LENGTH / sizeof(char);
+             i++) {
           files[idx] = filesInsideFolder[i];
           idx++;
         }
@@ -48,4 +54,10 @@ void readFolderFiles(char *dirname, char **files) {
   }
 }
 
-
+int compressFile(char *filename) {
+  char command[30] = "gzip ";
+  int result;
+  strcat(command, filename);
+  result = system(command);
+  return result;
+}
